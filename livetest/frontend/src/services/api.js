ab0c08cfeff6976b30127bff5799bc0ab5d52062
mukuhaben <254engineerben@gmail.com>
2025-06-15 18:33:43 +0300

    in frontend api.js changed from local host to api base url

--- livetest/frontend/src/services/api.js
+++ livetest/frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 // Create axios instance
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000/api",
+  baseURL: import.meta.env.VITE_API_URL || "https://firstcraft-backend-q68n.onrender.com/api",
   timeout: 10000,
   withCredentials: true,
 })
